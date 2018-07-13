77894a9c4885765da4003ce1e5488c746980b2b2
Nicholas Ferrara <nferrara100@gmail.com>
2018-07-13 20:26:31 +0100

    Object graphing stability

--- Mapper.py
+++ Mapper.py
@@ -1,13 +1,41 @@
-import ast, argparse
+import ast
+import argparse
+# import builtins
+
+
+class FuncNode:
+    _name = ""
+    _class_name = ""
+
+    def __init__(self, name="none", class_name="none"):
+        self._name = name
+        self._class_name = class_name
+
+    def get_name(self):
+        return self._name
+
+    def get_class(self):
+        return self._class_name
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_class(self, class_name):
+        self._class_name = class_name
 
 
 class FuncLister(ast.NodeVisitor):
-    current_func = "file"
+    funcNode = FuncNode()
     theGraph = {}
     built_ins = False
 
     def __init__(self, built_ins=False):
         self.built_ins = built_ins
+
+    def visit_ClassDef(self, node):
+        # print("Class " + node.name)
+        self.current_class = node.name
+        self.generic_visit(node)
 
     def visit_FunctionDef(self, node):
         # print("Def " + node.name)
@@ -17,10 +45,21 @@ class FuncLister(ast.NodeVisitor):
     def visit_Call(self, node):
         # print("Call " + node.func.id + " from " + self.current_func)
         # Should check here if it is a built-in function
+        # print("Current: " + self.current_func)
+        # print(ast.dump(node))
         if self.current_func in self.theGraph:
-            self.theGraph[self.current_func].append(node.func.id)
+            try:
+                self.theGraph[self.current_func].append([node.func.id, self.current_class])
+            except AttributeError:
+                self.theGraph[self.current_func].append([node.func.attr, self.current_class])
+                pass
         else:
-            self.theGraph[self.current_func] = [node.func.id]
+            try:
+                self.theGraph[self.current_func] = [[node.func.id], self.current_class]
+            except AttributeError:
+                print(ast.dump(node))
+                # self.theGraph[self.current_func] = [node.func.value.id]
+                pass
         self.generic_visit(node)
 
     def get_graph(self):
@@ -32,8 +71,12 @@ parser.add_argument('--filename', '-f', metavar='F', type=str, nargs=1,
                     help="Specify the name of the file to be examined.")
 parser.add_argument('--built-ins', '-b', action='store_true',
                     help="Also graph when Python's built in functions are used.")
+parser.add_argument('--debug', '-d', dest='debug', action='store_true', default=False,
+                    help="Enable debugging output.")
 
 args = parser.parse_args()
+DEBUG = args.debug
+
 if args.filename:
     filename = args.filename[0]
 else:
@@ -43,10 +86,11 @@ if filename[-3:] != ".py":
     filename += ".py"
 
 tree = ast.parse(open(filename).read())
-FuncLister(args.built_ins).visit(tree)
+lister = FuncLister(args.built_ins)
+lister.visit(tree)
 
-testGraph = FuncLister().get_graph()
+testGraph = lister.get_graph()
 for node in testGraph:
     for call in testGraph[node]:
-        print(node + " " + call)
+        print(node + " " + call[1] + "." + call[0])
 print("Complete")
--- TestFunctions.py
+++ TestFunctions.py
@@ -9,8 +9,47 @@ def function2():
 
 def function3():
     print("function3")
+    recursive_function(5)
 
 
 def function4():
     print("function4")
     function1()
+    outer_function()
+
+
+def recursive_function (remaining):
+    print("recursive_function " + remaining + " remaining")
+    if remaining > 0:
+        recursive_function(remaining-1)
+
+
+def outer_function():
+    print("outer_function")
+
+    def inner_function():
+        print("inner_function")
+        function2()
+
+    inner_function()
+
+
+class TestClass():
+
+    def __init__(self):
+        print("__init__")
+        self.method4()
+
+    def method1(self):
+        print("method1")
+
+    def method2(self):
+        print("method2")
+        self.method3()
+        function2()
+
+    def method3(self):
+        print("method3")
+
+    def method4(self):
+        print("method4")
