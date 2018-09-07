8b20d774bf7e2f02651d9f9c2b7567b8cf6aa5da
Nicholas Ferrara <nferrara100@gmail.com>
2018-09-07 20:45:47 +0100

    User friendly messages

--- mapper.py
+++ mapper.py
@@ -46,12 +46,10 @@ class Search:
                             help="also graph when Python's built in functions are used")
         parser.add_argument('--raw', '-r', action='store_true', default=False,
                             help="remove instruction text and formatting")
-        parser.add_argument('--metrics', '-m', action='store_true', default=False,
-                            help="print connectivity data")
+        parser.add_argument('--measurements', '-m', action='store_true', default=False,
+                            help="prints useful measurements about the relationships between functions")
         parser.add_argument('--draw', '-d', action='store_true', default=False,
                             help="save to result to a .png file in new subdirectory dependency_mapping/")
-        parser.add_argument('--no-refresh', '-n', action='store_true', default=False,
-                            help="exit after initial data print")
         args = parser.parse_args()
 
         # Processes the input parameters.
@@ -83,6 +81,7 @@ class Search:
     # Adds to the existing graph object.
     def append_graph(self, file):
         self.tree[file] = ast.parse(open(file).read())
+        # print(ast.dump(self.tree[file]))
         creator = NodeCreator(search=self, filename=file)
         creator.visit(self.tree[file])
         self.files.append(file)
@@ -106,18 +105,19 @@ class Search:
 
                 if len(self.crawled_imports) is not 0:
                     imports_str = ", ".join(sorted(self.crawled_imports))
-                    print("Also crawled: %s" % imports_str)
+                    print("Also crawled these imports: %s" % imports_str)
 
                 if len(self.uncrawled) is not 0:
                     uncrawled_str = ", ".join(sorted(self.uncrawled))
-                    print("Failed to crawl: %s" % uncrawled_str)
+                    print("Failed to crawl these imports: %s" % uncrawled_str)
 
                 if len(self.unsure_nodes) is not 0:
                     unsure_str = ", ".join(sorted(self.unsure_nodes))
-                    print("Ambiguous nodes: %s" % unsure_str)
+                    print("Could not include the following functions: %s" % unsure_str)
 
-                if self.args.metrics is True:
-                    self.print_metrics_str()
+                if self.args.measurements is True:
+                    print()
+                    self.print_measurements()
 
                 if self.args.inverse is True:
                     dependents_string = "Dependencies"
@@ -166,46 +166,60 @@ class Search:
             os.mkdir(directory)
         plt.savefig(directory + os.sep + filename)
 
-    def print_metrics_str(self):
-        degree_sequence = sorted([d for n, d in self.nxg.degree()], reverse=True)
-        max_degree = max(degree_sequence)
-        mean_degree = statistics.mean(degree_sequence)
-        # all_pairs_con = networkx.algorithms.approximation.connectivity.all_pairs_node_connectivity
-        # (self.nxg)
+    def print_experimental_measurements(self):
+        maximal_independent_set = networkx.algorithms.mis.maximal_independent_set(self.nxg.to_undirected())
+        degree_centrality = networkx.algorithms.centrality.degree_centrality(self.nxg)
+        edge_load_centrality = networkx.algorithms.centrality.edge_load_centrality(self.nxg)
+        global_reaching_centrality = networkx.algorithms.centrality.global_reaching_centrality(self.nxg)
         # Very slow
-        # average_connectivity = networkx.algorithms.connectivity.connectivity.
-        # average_node_connectivity(self.nxg)
-        edge_connectivity = networkx.algorithms.connectivity.connectivity. \
-            edge_connectivity(self.nxg.to_undirected())
-        node_connectivity = networkx.algorithms.connectivity.connectivity. \
-            node_connectivity(self.nxg.to_undirected())
+        average_connectivity = networkx.algorithms.connectivity.connectivity.average_node_connectivity(self.nxg)
+
+        print('Maximal independent set: ' + repr(maximal_independent_set))
+        print('Degree Centrality: ' + repr(degree_centrality))
+        print('Edge Load Centrality: ' + repr(edge_load_centrality))
+        print('Global reaching centrality: ' + repr(global_reaching_centrality))
+        print('Average Connectivity: {0:.2f}'.format(average_connectivity))
+
         is_connected = networkx.is_connected(self.nxg.to_undirected())
-        node_num = self.nxg.number_of_nodes()
-        # maximal_independent_set = networkx.algorithms.mis.maximal_independent_set
-        # (self.nxg.to_undirected())
-        # degree_centrality = networkx.algorithms.centrality.degree_centrality(self.nxg)
-        # edge_load_centrality = networkx.algorithms.centrality.edge_load_centrality(self.nxg)
-        # global_reaching_centrality = networkx.algorithms.centrality.global_reaching_centrality(self.nxg)
         degree_histogram = networkx.classes.function.degree_histogram(self.nxg)
         density = networkx.classes.function.density(self.nxg)
+        edge_connectivity = networkx.algorithms.connectivity.connectivity.edge_connectivity(self.nxg.to_undirected())
+
+        print('Degree Histogram: ' + repr(degree_histogram))
+        print('Density: ' + repr(density))
+        print('Is connected: ' + repr(is_connected))
+        print('Edge Connectivity: {0:.2f}'.format(edge_connectivity))
         if is_connected:
             minimum_edge_cut = networkx.algorithms.connectivity.cuts.minimum_edge_cut(self.nxg)
             print('Minimum edge cut: ' + repr(minimum_edge_cut))
 
-        print('Average Degree: {0:.2f}'.format(mean_degree))
-        print('Max Degree: ' + repr(max_degree))
-        # print('Average Connectivity: {0:.2f}'.format(average_connectivity))
-        print('Edge Connectivity: {0:.2f}'.format(edge_connectivity))
-        print('Node Connectivity: {0:.2f}'.format(node_connectivity))
-        print('Is connected: ' + repr(is_connected))
-        print('Number of nodes: ' + repr(node_num))
-        # print('Maximal independent set: ' + repr(maximal_independent_set))
-        # print('Degree Centrality: ' + repr(degree_centrality))
-        # print('Edge Load Centrality: ' + repr(edge_load_centrality))
-        # print('Global reaching centrality: ' + repr(global_reaching_centrality))
-        print('Degree Histogram: ' + repr(degree_histogram))
-        print('Density: ' + repr(density))
-        # print('Connectivity: ' + repr(all_pairs_con))
+    def print_measurements(self):
+        degree_sequence = sorted([d for n, d in self.nxg.degree()], reverse=True)
+        node_connectivity = networkx.algorithms.connectivity.connectivity.node_connectivity(self.nxg.to_undirected())
+        max_degree = max(degree_sequence)
+        mean_degree = statistics.mean(degree_sequence)
+        # all_pairs_con = networkx.algorithms.approximation.connectivity.all_pairs_node_connectivity(self.nxg)
+        all_pairs_con = networkx.algorithms.connectivity.connectivity.all_pairs_node_connectivity(
+            self.nxg.to_undirected())
+        node_num = self.nxg.number_of_nodes()
+        num_connected_nodes = 0
+        potential_pairs = 0
+        for node in all_pairs_con:
+            for pair in all_pairs_con[node]:
+                potential_pairs += 1
+                if all_pairs_con[node][pair] > 0:
+                    num_connected_nodes += 1
+        # print("Number of one-way node connections: " + repr(num_connected_nodes))
+        # print("Potential one-way node connections: " + repr(potential_pairs))
+        print('The average number of dependents and dependencies per function: {0:.2f}'.format(mean_degree))
+        print('The maximum number of dependents and dependencies per function: ' + repr(max_degree))
+        if node_connectivity == 0:
+            print('There are isolated functions or groups of isolated functions. Severity: {0:.2f}%'.format(
+                100 - 100 * (num_connected_nodes / potential_pairs)))
+        else:
+            print('There are no isolated functions or groups of isolated functions. At least {:d} function(s) that '
+                  'would need to be removed to isolate at least 1 function.'.format(node_connectivity))
+        print('Total functions found in the search area: ' + repr(node_num))
 
 
 # Represents function nodes in the graph.
